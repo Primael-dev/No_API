8f14fd8f04e15efd24ba5072f7284fbb38143b7b
Regina <reginaawadjihe38@gmail.com>
2026-01-07 20:43:32 +0100

    Implémentation du service authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,14 +1,68 @@
-const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
-const argon2 = require('argon2');
-const jwt = require('jsonwebtoken');
+import prisma from '#utils/prisma';
+import { hashPassword, verifyPassword } from '#utils/password';
+import { ConflictException, UnauthorizedException } from '#utils/exceptions';
 
-// Service pour gérer la logique de l'authentification
-
-	// Inscription d'un utilisateur
-	async function register(userData) {
-		// Vérification de l'existence de l'email dans la base
+export class AuthService {
+	// Inscription
+	static async register(userData){
 		const existingUser = await prisma.user.findUnique({
-			
-		})
+			where: {email: userData.email},
+		});
+
+		if (existingUser) {
+			throw new ConflictException("Cet email est déjà utilisé");
+		}
+
+		 const hashedPassword = await hashPassword(userData.password);
+
+		 return await prisma.user.create({
+			data: {
+				email: userData.email, 
+				password: hashedPassword,
+				firstName: userData.firstName,
+				lastName: userData.lastName,
+			},
+		});
 	}
+
+	// Connexion 
+	static async login(email, password, loginInfo){
+		const user = await prisma.user.findUnique({ where: {email }});
+
+		let isPasswordValid = false;
+
+		if(user){
+			isPasswordValid = await verifyPassword(user.password, password)
+		}
+
+		// Enregistrement dans l'historique
+		await prisma.loginHistory.create({
+			data: {
+				userId: user ? user.id : null,
+				ipAddress: loginInfo.ipAddress,
+				userAgent: loginInfo.userAgent,
+				success: (user !== null && isPasswordValid === true)
+			}
+		});
+
+		// Gestion de l'échec
+		if(!user || !isPasswordValid){
+			throw new UnauthorizedException("Identifiants incorrects");
+		}
+
+		return user;	
+	}
+
+	// Déconnexion
+	static async logout(userId, token){
+		if(!token) return;
+
+		return await prisma.blacklistedAccessToken.create({
+			data: {
+				token: token,
+				userId: userId,
+				expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+			},
+		});
+	}
+}
