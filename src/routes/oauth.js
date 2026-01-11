e43d36ec3d80bcbd9f70106073b8c7bd5f370a60
Kevin <lihounhintoe@gmail.com>
2026-01-11 20:50:46 +0100

    Ajout d'une page de redirection apres conection OAuth

--- src/routes/oauth.js
+++ src/routes/oauth.js
@@ -8,6 +8,7 @@ import {
 const router = express.Router();
 
 router.get("/google", googleRedirect);
-router.get("/google/callback", googleCallbacrouter.get("/google/success", oauthSuccess);
+router.get("/google/callback", googleCallback);
+router.get("/google/success", oauthSuccess);
 
 export default router;
